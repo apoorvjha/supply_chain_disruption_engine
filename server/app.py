b0f47d34f5b0bc800518f7a2ae9fa2e004e97391
TensorNd <do-not-mail@nothing.com>
2026-04-07 03:31:34 +0530

    QUirky fix

--- server/app.py
+++ server/app.py
@@ -89,4 +89,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
+    # main()
     main(port=args.port)
